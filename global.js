d7b064cb89bf7d0695d900e2750ff165d6ca5c18
Couvbat <92369131+Couvbat@users.noreply.github.com>
2022-03-15 16:58:59 +0100

    Add files via upload

---
+++ global.js
@@ -0,0 +1,20 @@
+function rainbow () {
+  let red = $('#red').val()
+  let green = $('#green').val()
+  let blue = $('#blue').val()
+  $("body").css('background-color', `rgb(${red},${green},${blue})`)
+}
+
+function effect () {
+  let red = 0
+  let green = 0
+  let blue = 0
+  switch (true) {
+    case (red == 255 && blue == 0)
+      break
+    default:
+      for(i=0; i<=255;i++)
+      break
+  }
+}
+
